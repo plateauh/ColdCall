1ea8482de049993af3e402f2ca2c65a7622bb1d4
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-06 21:25:00 +0300

    Done

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let codingDojoPeople = [
+        "Najd", "Kevin", "Hussein", "Rodrigo", "Danny", "Abdulaziz", "Najla", "Saud", "Lamis", "Ali", "Linah", "Reem", "Latifah", "Amnah", "Amal", "Hassan", "Hajar", "Shahad", "Mohammed", "Maha", "Areej", "Kholod", "Abdulrahman", "Smaher", "Aisha", "Asayil", "Hessa", "Laila", "Lubabah", "Reenad", "Rehab", "Safa", "Zahra"
+    ]
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func coldCallPressed(_ sender: UIButton) {
+        let randomIdx = Int.random(in: 0..<codingDojoPeople.count)
+        nameLabel.text = codingDojoPeople[randomIdx]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameLabel.text = codingDojoPeople[0] // sorry I want to be the first always haha
     }
 
 
